418dae216f5dba1d92ef32265e420ad97da1b041
Patrick Browning <patrick@browninglogic.com>
2019-02-09 19:08:59 -0500

    Add more dummy station data

--- src/app/components/stations/stations.component.ts
+++ src/app/components/stations/stations.component.ts
@@ -13,7 +13,12 @@ export class StationsComponent {
 
   public stations: Array<Station> = [
     { title: 'BBC World Service', url: 'http://bbcwssc.ic.llnwd.net/stream/bbcwssc_mp1_ws-eieuk', genre: 'News'},
-    { title: 'Radio Metal On: The Heavy', url: 'http://188.165.212.92:8000/heavy128mp3', genre: 'Heavy Metal'}
+    { title: 'Radio Metal On: The Heavy', url: 'http://188.165.212.92:8000/heavy128mp3', genre: 'Heavy Metal'},
+    { title: 'Radio Metal On: The Thrasher', url: 'http://188.165.212.92:8020/thrasher128mp3', genre: 'Thrash Metal' },
+    { title: 'Radio Caprice Speed Metal', url: 'http://79.111.119.111:9107/;', genre: 'Speed Metal' },
+    { title: 'Radio Caprice Thrash Metal', url: 'http://79.120.77.11:9101/;', genre: 'Thrash Metal' },
+    { title: 'Radio Caprice Heavy Metal', url: 'http://79.111.14.76:9061/;', genre: 'Heavy Metal' },
+    { title: 'Radio Caprice Power Metal', url: 'http://79.120.77.11:9099/;', genre: 'Power Metal' }
   ]
 
   onRowClicked(station: Station) {
