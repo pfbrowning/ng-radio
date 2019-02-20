6989698e5d4cc9d7b3204e13f710895c895819ae
Patrick Browning <patrick@browninglogic.com>
2019-02-19 23:42:54 -0500

    Implement now-playing component

--- src/app/components/now-playing/now-playing.component.ts
+++ src/app/components/now-playing/now-playing.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { PlayerService } from 'src/app/services/player.service';
+import { Subscription } from 'rxjs';
+import { NowPlaying } from 'src/app/models/now-playing';
+import { Utils } from 'src/app/utils/utils';
 
 @Component({
-  selector: 'app-now-playing',
   templateUrl: './now-playing.component.html',
   styleUrls: ['./now-playing.component.scss']
 })
-export class NowPlayingComponent implements OnInit {
+export class NowPlayingComponent implements OnInit, OnDestroy {
+  constructor(public playerService: PlayerService) {}
 
-  constructor() { }
+  public nowPlaying: NowPlaying;
+  private nowPlayingSubscription: Subscription;
 
   ngOnInit() {
+    this.nowPlayingSubscription = this.playerService.nowPlaying$
+      .subscribe(nowPlaying => this.nowPlaying = nowPlaying);
   }
 
+  ngOnDestroy() {
+    if(this.nowPlayingSubscription) this.nowPlayingSubscription.unsubscribe();
+  }
+
+  public onImgError(img: HTMLImageElement, altSrc: string) {
+    Utils.SetAltImage(img, altSrc);
+  }
 }
---
+++ src/app/utils/utils.ts
@@ -0,0 +1,7 @@
+export class Utils {
+    public static SetAltImage(img: HTMLImageElement, altSrc: string) {
+        if(img.src != altSrc) {
+            img.src = altSrc;
+        }
+    }
+}
