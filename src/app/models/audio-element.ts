e58d731634342ad343d1d5bf8eb69e09bc4a5590
Patrick Browning <patrick@browninglogic.com>
2019-04-17 22:34:47 -0400

    Audio element 'continued download while paused' fix

--- src/app/models/audio-element.ts
+++ src/app/models/audio-element.ts
@@ -7,23 +7,35 @@ export class AudioElement {
         private readonly audio: HTMLAudioElement = new Audio()
     ) {
         this.audio.onerror = (error) => this.error.emit(error);
-        this.audio.onpause = () => this.paused.emit();
         this.audio.onplaying = () => this.playing.emit();
+        this.audio.onpause = () => {
+            this.paused.emit();
+            /* Clear the src on the audio element in order
+            to prevent the browser from continuing to download
+            audio while paused. */
+            this.audio.src = '';
+        }
+        this.audio.preload = 'none';
     }
 
+    /* Maintain the audio source in a separate property
+    so that we can clear it from the audio element on pause
+    and then re-assign it on play. */
+    private _source : string;
     public error = new EventEmitter<any>();
     public playing = new EventEmitter<void>();
     public paused = new EventEmitter<void>();
 
     public get source(): string {
-        return this.audio.src;
+        return this._source;
     }
 
     public set source(value: string) {
-        this.audio.src = value;
+        this._source = value;
     }
 
     public play(): void {
+        this.audio.src = this._source;
         this.audio.play();
     }
 
