35bdf0f535b1c329585f5ab4bcb4b09979660cd7
Patrick Browning <patrick@browninglogic.com>
2019-04-17 21:41:50 -0400

    Marquee logic optimization
    Updated marquee logic such that we're now only checking the marquee
    after the change detection operation immediately following a NowPlaying
    model change or a window resize event.

--- src/app/components/player-bar-station-info/player-bar-station-info.component.ts
+++ src/app/components/player-bar-station-info/player-bar-station-info.component.ts
@@ -1,4 +1,4 @@
-import { Component, ViewChild, ElementRef, AfterViewChecked, ChangeDetectorRef, Input, OnChanges } from '@angular/core';
+import { Component, ViewChild, ElementRef, AfterViewChecked, ChangeDetectorRef, Input, OnChanges, SimpleChanges, OnInit, OnDestroy } from '@angular/core';
 import { NowPlaying } from 'src/app/models/now-playing';
 import { StreamInfoStatus } from 'src/app/models/stream-info-status';
 
@@ -7,21 +7,40 @@ import { StreamInfoStatus } from 'src/app/models/stream-info-status';
   templateUrl: './player-bar-station-info.component.html',
   styleUrls: ['./player-bar-station-info.component.scss']
 })
-export class PlayerBarStationInfoComponent implements AfterViewChecked {
+export class PlayerBarStationInfoComponent implements AfterViewChecked, OnChanges, OnInit, OnDestroy {
   constructor(private changeDetectorRef: ChangeDetectorRef) {}
 
   public streamInfoStatus = StreamInfoStatus;
   public titleMarquee = false;
   public stationMarquee = false;
+  private checkMarquee = false;
   @Input() nowPlaying: NowPlaying;
   @ViewChild('title') titleElement: ElementRef;
   @ViewChild('station') stationElement: ElementRef;
 
+  ngOnInit() {
+    /* Set checkmarquee on all window resize events because the marquee
+    needs to be re-evaluated for each screen size. */
+    window.onresize = () => this.checkMarquee = true;
+  }
+
+  ngOnDestroy() {
+    window.onresize = null;
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    this.checkMarquee = true;
+  }
+
   ngAfterViewChecked() {
-    /* Check and apply marquee classes if necessary.  This must happen immediately
-    after change detection because we won't know whether the content is overflowing
-    until it's been bound to the template. */
-    this.checkApplyMarquees();
+    /* Check and apply marquee classes immediately after a change detection operation in
+    which either now playing input model or the screen size just changed.  This must 
+    happen immediately after change detection because we won't know whether the 
+    content is overflowing until it's been bound to the template. */
+    if(this.checkMarquee) {
+      this.checkMarquee = false;
+      this.checkApplyMarquees();
+    }
   }
 
   /** Checks and updates the marquee properties for title and station
