6d8a3e9c14602fd3bad7dfbd1523ee2225630841
Patrick Browning <patrick@browninglogic.com>
2019-04-17 21:40:19 -0400

    Async pipe for play button in player bar

--- src/app/components/player-bar/player-bar.component.ts
+++ src/app/components/player-bar/player-bar.component.ts
@@ -3,43 +3,20 @@ import { PlayerService } from 'src/app/services/player.service';
 import { SleepTimerService } from 'src/app/services/sleep-timer.service';
 import { NoSleepService } from 'src/app/services/no-sleep.service';
 import { Subscription } from 'rxjs';
+import { Utils } from 'src/app/utils/utils';
 
 @Component({
   selector: 'blr-player-bar',
   templateUrl: './player-bar.component.html',
   styleUrls: ['./player-bar.component.scss']
 })
-export class PlayerBarComponent implements OnInit, OnDestroy {
+export class PlayerBarComponent {
   constructor(public playerService: PlayerService,
     public sleepTimerService: SleepTimerService,
-    public noSleepService: NoSleepService,
-    private changeDetectorRef: ChangeDetectorRef ) {}
-
-  public audioPaused: boolean;
-  private audioPausedSubscription: Subscription;
-
-  ngOnInit() {
-    // When the pause state changes
-    this.audioPausedSubscription = this.playerService.paused.subscribe(paused => {
-      // Bind the new value to the component for template binding
-      this.audioPaused = paused;
-      // Explicitly trigger change detection because Angular doesn't do it for us on Audio events
-      this.changeDetectorRef.detectChanges();
-    });
-  }
-
-  ngOnDestroy() {
-    if (this.audioPausedSubscription) { this.audioPausedSubscription.unsubscribe(); }
-  }
+    public noSleepService: NoSleepService) {}
 
   public onImgError(img: HTMLImageElement) {
-    /* If the image didn't load properly, then use a default
-    alternative image in its place.  However, first check to ensure
-    that it's not the default image itself that's erroring out. */
-    const altImage = '/assets/images/radio.svg';
-    if (img.src !== altImage) {
-      img.src = altImage;
-    }
+    Utils.SetAltImage(img, '/assets/images/radio.svg');
   }
 
   public onTimerSelected(length: number) {
--- src/app/utils/utils.ts
+++ src/app/utils/utils.ts
@@ -1,4 +1,7 @@
 export class Utils {
+    /** Sets an alternative image source on the provided image element
+     * if the alt isn't already assigned.  Useful for using an alt image
+     * in case the requested image fails to load. */
     public static SetAltImage(img: HTMLImageElement, altSrc: string) {
         if (img.src !== altSrc) {
             img.src = altSrc;
