88984f51c5da2af4f1786eb1413e08451030f2b2
Patrick Browning <patrick@browninglogic.com>
2019-04-18 23:31:30 -0400

    Go to now-playing on now-playing toolbar click
    Updated so that we're navigating to the now-playing route when the user
    clicks on the now-playing info in the toolbar and they're not already at
    the now-playing route.

--- src/app/components/player-bar/player-bar.component.spec.ts
+++ src/app/components/player-bar/player-bar.component.spec.ts
@@ -1,4 +1,5 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 import { PlayerBarComponent } from './player-bar.component';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatToolbarModule } from '@angular/material/toolbar';
@@ -23,6 +24,7 @@ describe('PlayerBarComponent', () => {
         PlayerBarStationInfoComponent
       ],
       imports: [
+        RouterTestingModule,
         MatMenuModule,
         MatToolbarModule,
         MatButtonModule,
--- src/app/components/player-bar/player-bar.component.ts
+++ src/app/components/player-bar/player-bar.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 import { PlayerService } from 'src/app/services/player.service';
 import { SleepTimerService } from 'src/app/services/sleep-timer.service';
 import { KeepAwakeService } from 'src/app/services/keep-awake.service';
@@ -12,7 +13,8 @@ import { Utils } from 'src/app/utils/utils';
 export class PlayerBarComponent {
   constructor(public playerService: PlayerService,
     public sleepTimerService: SleepTimerService,
-    public keepAwakeService: KeepAwakeService) {}
+    public keepAwakeService: KeepAwakeService,
+    private router: Router) {}
 
   public onImgError(img: HTMLImageElement) {
     Utils.SetAltImage(img, '/assets/images/radio.svg');
@@ -23,6 +25,16 @@ export class PlayerBarComponent {
       this.sleepTimerService.setTimer(length);
     } else {
       this.sleepTimerService.cancelTimer();
+    }
+  }
+
+  public onNowPlayingClicked(): void {
+    /* When the user clicks on the now playing info in the player bar,
+    take them to the now-playing component if they're not already
+    there. */
+    if (this.router.url !== '/now-playing') {
+      console.log('navigating');
+      this.router.navigate(['/now-playing']);
     }
   }
 }
