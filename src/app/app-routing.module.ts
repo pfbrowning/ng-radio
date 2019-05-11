40ba76e9da3f2a59f372d84c8ec24b57cbe96d19
Patrick Browning <patrick@browninglogic.com>
2019-05-11 11:44:45 -0400

    Fix routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,8 @@ import { AuthGuard } from '@modules/core/authentication/authentication.module';
 
 const routes: Routes = [
   // Check to ensure that the user is authenticated before loading the radio app module
-  { path: '', canActivate: [ AuthGuard ], loadChildren: '../modules/lazy/radio-app/radio-app.module#RadioAppModule' }
+  { path: 'app', canActivate: [ AuthGuard ], loadChildren: '../modules/lazy/radio-app/radio-app.module#RadioAppModule' },
+  { path: '', redirectTo: 'app', pathMatch: 'full'}
 ];
 
 @NgModule({
