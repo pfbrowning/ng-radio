b011caccf65fb1b9f2907f5a0d18bc05d42bb8e6
unknown <patrick@browninglogic.com>
2020-04-04 17:36:41 -0400

    Test CORS proxy with prod origin

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,8 @@
-import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { Component, ChangeDetectionStrategy, OnInit } from '@angular/core';
 import { Store, select } from '@ngrx/store';
 import { RootState } from '@core';
 import { selectIsStationSelected } from '@core/store/player';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'blr-root',
@@ -9,8 +10,17 @@ import { selectIsStationSelected } from '@core/store/player';
   styleUrls: ['./app.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class AppComponent {
-  constructor(private store: Store<RootState>) {}
+export class AppComponent implements OnInit {
+  constructor(private store: Store<RootState>, private httpClient: HttpClient) {}
 
   public stationSelected$ = this.store.pipe(select(selectIsStationSelected));
+
+  public ngOnInit(): void {
+    this.httpClient.get('https://thingproxy.freeboard.io/fetch/http://79.111.119.111:9107/7.html',
+    {responseType:'text'})
+    .subscribe({
+      next: next => console.log('fetched',  next),
+      error: error => console.log('error', error)
+    });
+  }
 }
