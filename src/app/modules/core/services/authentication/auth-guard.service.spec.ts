75bba1af4a81baf0e1ae9f2a75702c7218d986f1
Patrick Browning <patrick@browninglogic.com>
2020-06-22 22:11:49 -0400

    Service tests

--- src/app/modules/core/services/authentication/auth-guard.service.spec.ts
+++ src/app/modules/core/services/authentication/auth-guard.service.spec.ts
@@ -1,22 +1,19 @@
 import { TestBed } from '@angular/core/testing';
 import { AuthGuardService } from './auth-guard.service';
 import { OAuthService } from 'angular-oauth2-oidc';
-import { RootState } from '../../models/root-state';
-import { MockStore, provideMockStore } from '@ngrx/store/testing';
+import { provideMockStore } from '@ngrx/store/testing';
 import { CoreSpyFactories } from '@core/testing';
 import { initialRootState } from '../../models/initial-root-state';
-import { initialAuthenticationState } from '../../models/authentication/initial-authentication-state';
 import { AuthenticationService } from './authentication.service';
 import { AuthenticationServiceStub } from '../../testing/stubs/authentication-service-stub.spec';
+import { hot, cold } from 'jasmine-marbles';
 
 describe('AuthGuardService', () => {
   let authGuardService: AuthGuardService;
-  let canActivateSpy;
   let oauthService: jasmine.SpyObj<OAuthService>;
   let authenticationService: AuthenticationServiceStub;
 
   beforeEach(() => {
-    canActivateSpy = jasmine.createSpyObj('canActivate', ['next', 'error', 'complete']);
     oauthService = CoreSpyFactories.createOAuthServiceSpy();
 
     TestBed.configureTestingModule({
@@ -29,36 +26,23 @@ describe('AuthGuardService', () => {
 
     authGuardService = TestBed.inject(AuthGuardService);
     authenticationService = TestBed.inject(AuthenticationService) as any;
-
-    authGuardService.canActivate().subscribe(
-      value => canActivateSpy.next(value),
-      error => canActivateSpy.error(error),
-      () => canActivateSpy.complete()
-    );
   });
 
   it('should allow entry for an authenticated user', () => {
-    expect(canActivateSpy.next).not.toHaveBeenCalled();
+    // Arrange
+    authenticationService.authenticated$ = hot('-(a|)', { a: true });
 
-    // Arrange & Act
-    authenticationService.authenticatedSubject.next(true);
-
-    // Assert: Ensure that 'true' was emitted and that we didn't redirect
-    expect(canActivateSpy.next).toHaveBeenCalledTimes(1);
-    expect(canActivateSpy.next.calls.mostRecent().args[0]).toBe(true);
-    expect(canActivateSpy.error).not.toHaveBeenCalled();
-    expect(canActivateSpy.complete).toHaveBeenCalledTimes(1);
+    // Act & Assert
+    expect(authGuardService.canActivate()).toBeObservable(cold('-(a|)', { a: true }));
     expect(oauthService.initCodeFlow).not.toHaveBeenCalled();
   });
 
   it('should redirect and never complete canActivate for a non-authenticated user', () => {
-    // Arrange & Act
-    authenticationService.authenticatedSubject.next(false);
+    // Arrange
+    authenticationService.authenticated$ = hot('-(a|)', { a: false });
 
-    // Assert: Ensure that 'true' was emitted and that we initiated redirect to IDP
-    expect(canActivateSpy.next).not.toHaveBeenCalled();
-    expect(canActivateSpy.error).not.toHaveBeenCalled();
-    expect(canActivateSpy.complete).not.toHaveBeenCalled();
+    // Act & Assert
+    expect(authGuardService.canActivate()).toBeObservable(cold('--'));
     expect(oauthService.initCodeFlow).toHaveBeenCalledTimes(1);
   });
 });
--- src/app/modules/core/services/authentication/authentication.service.spec.ts
+++ src/app/modules/core/services/authentication/authentication.service.spec.ts
@@ -1,11 +1,14 @@
-import { TestBed } from '@angular/core/testing';
+import { TestBed, fakeAsync, tick } from '@angular/core/testing';
 
 import { AuthenticationService } from './authentication.service';
-import { provideMockStore } from '@ngrx/store/testing';
+import { provideMockStore, MockStore } from '@ngrx/store/testing';
 import { initialRootState } from '../../models/initial-root-state';
+import { RootState } from '../../models/root-state';
+import { AuthenticationSelectors } from '../../store/authentication';
 
 describe('AuthenticationService', () => {
-  let service: AuthenticationService;
+  let authenticationService: AuthenticationService;
+  let store: MockStore<RootState>;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
@@ -13,10 +16,37 @@ describe('AuthenticationService', () => {
         provideMockStore({initialState: initialRootState})
       ]
     });
-    service = TestBed.inject(AuthenticationService);
+    authenticationService = TestBed.inject(AuthenticationService);
+    store = TestBed.inject(MockStore);
+  });
+
+  afterEach(() => {
+    store.resetSelectors();
   });
 
   it('should be created', () => {
-    expect(service).toBeTruthy();
+    expect(authenticationService).toBeTruthy();
+  });
+
+  describe('authenticated$', () => {
+    it('should wait until auth initializes before emitting and completing', fakeAsync(() => {
+      // Arrange
+      const authenticatedSpy = jasmine.createSpyObj('authenticated$', [ 'next', 'complete' ]);
+      authenticationService.authenticated$.subscribe({
+        next: val => authenticatedSpy.next(val),
+        complete: () => authenticatedSpy.complete()
+      });
+      expect(authenticatedSpy.next).not.toHaveBeenCalled();
+
+      // Act
+      tick(200);
+      store.overrideSelector(AuthenticationSelectors.initializationState, { initialized: true, authenticated: true });
+      store.refreshState();
+
+      // Assert
+      expect(authenticatedSpy.next).toHaveBeenCalledTimes(1);
+      expect(authenticatedSpy.next.calls.mostRecent().args[0]).toBe(true);
+      expect(authenticatedSpy.complete).toHaveBeenCalledTimes(1);
+    }));
   });
 });
