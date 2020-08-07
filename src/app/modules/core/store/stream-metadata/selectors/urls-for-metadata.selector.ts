ed594975110795eb07ff55eb2f6e0c826e7058a0
unknown <patrick@browninglogic.com>
2020-08-07 14:12:56 -0400

    Include favorite stations based on route

--- src/app/modules/core/store/stream-metadata/selectors/urls-for-metadata.selector.ts
+++ src/app/modules/core/store/stream-metadata/selectors/urls-for-metadata.selector.ts
@@ -2,18 +2,28 @@ import { createSelector } from '@ngrx/store';
 import { PlayerStatus } from '../../../models/player/player-status';
 import * as PlayerSelectors from '../../player/player.selectors';
 import * as RouterSelectors from '../../router/selectors';
+import * as FavoritesSelectors from '../../favorite-stations/favorite-stations.selectors';
 
 export const urlsSelectedForMetadata = createSelector(
     PlayerSelectors.selectCurrentStation,
     PlayerSelectors.selectPlayerStatus,
     RouterSelectors.currentUrl,
-    (currentStation, currentStatus, currentRoute) => {
+    FavoritesSelectors.selectFavoriteStations,
+    (currentStation, currentStatus, currentRoute, favorites) => {
         /* TODO Conditionally add the urls for favorites or search based on the current
         route url. */
         console.log('current route', currentRoute);
-        const initial = currentStation != null && currentStatus == PlayerStatus.Playing
-            ? [ currentStation.url ]
-            : []
-        return initial;
+        const urls = [];
+        if (currentStation != null && currentStatus == PlayerStatus.Playing) {
+            urls.push(currentStation.url);
+        }
+        switch(currentRoute) {
+            case '/favorites':
+                if (favorites) {
+                    urls.push(...favorites.map(s => s.url));
+                }
+                break;
+        }
+        return urls;
     }
 )
