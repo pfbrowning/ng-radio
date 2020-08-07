082279ec071be46b4f6b28496e2be68af0e85188
unknown <patrick@browninglogic.com>
2020-08-07 13:07:11 -0400

    distinctUntilChanged setStreamList$

--- src/app/modules/core/store/stream-metadata/effects.ts
+++ src/app/modules/core/store/stream-metadata/effects.ts
@@ -1,9 +1,10 @@
 import { Injectable } from '@angular/core';
 import { Actions, createEffect, ofType } from '@ngrx/effects';
-import { map, switchMap, catchError, concatMap, debounceTime} from 'rxjs/operators';
+import { map, switchMap, catchError, concatMap, debounceTime, distinctUntilChanged} from 'rxjs/operators';
 import { of } from 'rxjs';
 import { StreamMetadataService } from '@core/services';
 import { StreamMetadataFacadeService } from './stream-metadata-facade.service';
+import { isEqual } from 'lodash-es';
 import * as StreamMetadataActions from './actions';
 
 @Injectable()
@@ -28,6 +29,7 @@ export class StreamMetadataEffects {
 
   setStreamList$ = createEffect(() => this.streamMetadataFacade.urlsSelectedForMetadata$.pipe(
     debounceTime(250),
+    distinctUntilChanged((x, y) => isEqual(x, y)),
     map(streams => StreamMetadataActions.setStreamListStart({ streams }))
   ));
 }
