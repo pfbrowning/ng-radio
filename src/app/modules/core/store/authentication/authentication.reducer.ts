691d1ede88408fd1c27ce835b8322792e88ef1cc
unknown <patrick@browninglogic.com>
2020-10-16 18:11:34 -0400

    Fix reducer export

--- src/app/modules/core/store/authentication/authentication.reducer.ts
+++ src/app/modules/core/store/authentication/authentication.reducer.ts
@@ -3,7 +3,7 @@ import { AuthenticationState } from './models/authentication-state';
 import { initialAuthenticationState } from './models/initial-authentication-state';
 import * as Actions from './authentication.actions';
 
-export const reducer = createReducer<AuthenticationState>(
+export const authenticationReducer = createReducer<AuthenticationState>(
     initialAuthenticationState,
     on(
         Actions.initializeSucceeded,
