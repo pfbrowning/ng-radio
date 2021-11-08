6ea9722b6cdd6edfc579b0504d1257d7f1a09639
Patrick Browning III <patrick@browninglogic.com>
2021-11-07 21:43:20 -0500

    fix: replace karma-coverage-istanbul-reporter with karma-coverage

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -12,16 +12,16 @@ module.exports = function (config) {
       require('karma-chrome-launcher'),
       require('karma-firefox-launcher'),
       require('karma-jasmine-html-reporter'),
-      require('karma-coverage-istanbul-reporter'),
+      require('karma-coverage'),
       require('@angular-devkit/build-angular/plugins/karma'),
       require('karma-junit-reporter')
     ],
     client: {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
-    coverageIstanbulReporter: {
+    coverageReporter: {
       dir: require('path').join(__dirname, '../coverage'),
-      reports: ['html', 'lcov', 'cobertura'],
+      reporters: ['html', 'lcov', 'cobertura'],
       fixWebpackSourcePaths: true
     },
     reporters: ['progress', 'kjhtml'],
